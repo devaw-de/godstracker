dd8cab15f118068892884faf5792e7db8b168c0b
devawde <it@alex-will.de>
2025-09-20 12:49:04 +0200

    added faq content

--- src/app/feature/menu/faq/faq.ts
+++ src/app/feature/menu/faq/faq.ts
@@ -1,10 +1,42 @@
 export const FAQ = [
   {
     title: 'Why is this so ugly?',
-    answer: 'I am not a designer'
+    answers: ['I am not a designer']
   },
   {
     title: 'Is this free?',
-    answer: 'Absolutely. Use the app, check out the code for the app on github, change it to meet your needs or ignore it'
+    answers: [
+      'Absolutely.',
+      'Use the app, check out the code for the app on github, change it to meet your needs or ignore it'
+    ]
+  },
+  {
+    title: 'How do I use this app?',
+    answers: [
+      'Open a location and enter any quests you receive (rewards) or quests you need (requirements).',
+      'You can also tag or comment on a location.',
+      'Furthermore, the app can be used to simplify "saving" your game state between sessions. Check out the Crew and Ship sections for that',
+      'When you open the app the next time, anything you entered will still be there, provided you are using the same device/browser.'
+    ],
+  },
+  {
+    title: 'Ok, but what if want to use a different device?',
+    answers: [
+      'Open the menu and click on the "export" button to export your progress.',
+      'Transfer the downloaded file to your new device, bring up the app and import the file.'
+    ]
+  },
+  {
+    title: 'That is too complicated. Can I just sign up somewhere?',
+    answers: [
+      'No.',
+      'Sleeping Gods Tracker is a privacy-first kind of app. Nothing you do is sent to the cloud or anywhere else. Things happen on your device and that is it.'
+    ]
+  },
+  {
+    title: 'I want another feature',
+    answers: [
+      'Feel free to open a ticket on github.'
+    ]
   }
 ];
